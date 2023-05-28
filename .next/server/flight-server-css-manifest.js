1d05a8f2a3b51c48b67478b691a5f05672649f5c
Paul Lai <plutodark@gmail.com>
2023-05-28 13:42:06 +0100

    update gitignore

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/paul/Documents/code/cv-next/src/app/layout.tsx": [
+      "/Users/paul/Documents/code/cv-next/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"weight\":\"400\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/paul/Documents/code/cv-next/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/paul/Documents/code/cv-next/src/app/page": [
+      "/Users/paul/Documents/code/cv-next/src/app/globals.css",
+      "/Users/paul/Documents/code/cv-next/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"weight\":\"400\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,10 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/Users/paul/Documents/code/cv-next/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"weight\":\"400\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": [
+      "static/media/457c19822e909381-s.p.woff2"
+    ]
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
